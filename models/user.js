57262cc47b1e6122abecf4de262c249acd89b650
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-10-04 12:31:07 -0700

    added all, get and messagesFrom methods

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@
 
 const e = require("express");
 const db = require("../db");
-const { BCRYPT_WORK_FACTOR } = require("../config")
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const { NotFoundError } = require("../expressError");
 
 /** User of the site. */
 
@@ -51,17 +52,29 @@ class User {
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
-    await db.query(
+
+    const result = await db.query(
       `UPDATE users
        SET last_login_at = current_timestamp
-         WHERE username = $1`,
+         WHERE username = $1
+         RETURNING username, last_login_at`,
       [username]);
+
+    user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No such user: ${username}`);
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const result = await db.query(
+      `SELECT username, first_name, last_name
+          FROM users
+      `
+    );
+    return result.rows;
   }
 
   /** Get: get user by username
@@ -74,6 +87,17 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const result = await db.query(
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at
+          FROM users
+          WHERE username = $1
+      `, [username]
+    );
+    
+    user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No such user: ${username}`);
+    return user;
   }
 
   /** Return messages from this user.
@@ -85,6 +109,22 @@ class User {
    */
 
   static async messagesFrom(username) {
+    const result = await db.query(
+      `SELECT m.id AS "id",
+              m.to_user AS "to_user",
+              m.body AS "body",
+              m.sent_at AS "sent_at",
+              m.read_at AS "read_at"
+          FROM messages as m
+              JOIN users AS f ON m.from_username = f.username
+              JOIN users AS t ON m.to_username = t.username
+          WHERE m.from_username = $1
+      `, [username]
+    );
+
+    // we need to write another query for to_user
+    // append to_user to return message
+    
   }
 
   /** Return messages to this user.
