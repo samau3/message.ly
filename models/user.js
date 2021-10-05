2b51042446686ea1ee5a9ebe51af89b1a264855f
samau3 <samau415@gmail.com>
2021-10-05 11:34:14 -0700

    updated messagesTo and messagesFrom to return based on docstring output, completed users routes

--- models/user.js
+++ models/user.js
@@ -112,35 +112,70 @@ class User {
    */
 
   static async messagesFrom(username) {
-    const result = await db.query(
-      `SELECT m.id AS id,
-              m.to_username AS to_username,
-              m.body AS body,
-              m.sent_at AS sent_at,
-              m.read_at AS read_at,
-              t.first_name AS to_first_name,
-              t.last_name AS to_last_name, 
-              t.phone AS to_phone
-          FROM messages as m
-              JOIN users AS t ON m.to_username = t.username
-          WHERE m.from_username = $1
+    // const result = await db.query(
+    //   `SELECT m.id AS id,
+    //           m.to_username AS to_username,
+    //           m.body AS body,
+    //           m.sent_at AS sent_at,
+    //           m.read_at AS read_at,
+    //           t.first_name AS to_first_name,
+    //           t.last_name AS to_last_name, 
+    //           t.phone AS to_phone
+    //       FROM messages as m
+    //           JOIN users AS t ON m.to_username = t.username
+    //       WHERE m.from_username = $1
+    //   `, [username]
+    // );
+    // const u = result.rows[0];
+
+    // if (!u) throw new NotFoundError(`No such user: ${username}`);
+    // return [{
+    //   id: u.id,
+    //   to_user: {
+    //     username: u.to_username,
+    //     first_name: u.to_first_name,
+    //     last_name: u.to_last_name,
+    //     phone: u.to_phone,
+    //   },
+    //   body: u.body,
+    //   sent_at: u.sent_at,
+    //   read_at: u.read_at,
+    // }];
+
+    const messageResult = await db.query(
+      `SELECT id, to_username AS to_user, body, sent_at, read_at
+          FROM messages
+          WHERE from_username = $1
       `, [username]
     );
-    const u = result.rows[0];
 
-    if (!u) throw new NotFoundError(`No such user: ${username}`);
-    return [{
-      id: u.id,
-      to_user: {
-        username: u.to_username,
-        first_name: u.to_first_name,
-        last_name: u.to_last_name,
-        phone: u.to_phone,
-      },
-      body: u.body,
-      sent_at: u.sent_at,
-      read_at: u.read_at,
-    }];
+    const messages = messageResult.rows; // [{id, from_user, body, sent_at, read_at}, {id, from_user, body, sent_at, read_at}]
+    if (messages.length === 0) throw new NotFoundError(`No messages from: ${username}`);
+
+    const fromUser = messages.map(m => m.to_user);
+    const formattedUser = fromUser.join();
+
+    const userResult = await db.query(
+      `SELECT username, first_name, last_name, phone
+          FROM users
+          WHERE username IN ($1)
+      `, [formattedUser]
+    );
+
+    const users = new Set(userResult.rows);
+    const usersEntries = users.entries();
+    const finalResult = messages.map(m => {
+
+      for (let entry of usersEntries) {
+        if (entry[0].username === m.to_user) {
+          m.to_user = entry[0];
+        }
+      }
+      return m;
+    });
+    return finalResult;
+
+
   }
 
 
@@ -174,15 +209,19 @@ class User {
     // potentially have to loop the results.row and set the objects to be 
     // in the format below via a map
     const messageResult = await db.query(
-      `SELECT id, from_username, body, sent_at, read_at
+      `SELECT id, from_username AS from_user, body, sent_at, read_at
           FROM messages
           WHERE to_username = $1
       `, [username]
     );
 
     const messages = messageResult.rows; // [{id, from_user, body, sent_at, read_at}, {id, from_user, body, sent_at, read_at}]
-    const fromUser = messages.map(m => m.from_username);
+
+    if (messages.length === 0) throw new NotFoundError(`No messages to: ${username}`);
+
+    const fromUser = messages.map(m => m.from_user);
     const formattedUser = fromUser.join();
+    // console.log('formattedUser', formattedUser)
 
 
     const userResult = await db.query(
@@ -192,19 +231,26 @@ class User {
       `, [formattedUser]
     );
 
-    const users = new Set (userResult); 
-    const usersEntries = users.entries(); // [{username, first_name, last_name, phone}, {username, first_name, last_name, phone}]
-
+    // console.log('userResult', userResult.rows)
+    const users = new Set(userResult.rows);
+    const usersEntries = users.entries();
+    // console.log("userEntries", usersEntries)// [{username, first_name, last_name, phone}, {username, first_name, last_name, phone}]
     const finalResult = messages.map(m => {
-      
+
       for (let entry of usersEntries) {
-        if (entry[0][username] === m.from_username) {
-          m.from_username = entry[0];
+        // console.log("entry", entry)
+        // console.log("entry0username", entry[0].username)
+        // console.log("m.fromUsername", m.from_username)
+        if (entry[0].username === m.from_user) {
+          // console.log("in if entry0username", entry[0].username)
+          // console.log("before m.from", m.from_username)
+          m.from_user = entry[0];
+          // console.log("after m.from", m.from_username)
         }
       }
       return m;
     });
-    console.log("finalResult", finalResult);
+    // console.log("finalResult", finalResult);
     return finalResult;
 
     // alternatively, do two database queries
--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,28 @@
 const Router = require("express").Router;
 const router = new Router();
 const User = require("../models/user");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
 
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
-
+router.get("/", ensureLoggedIn, async function (req, res, next) {
+    const users = await User.all();
+    return res.json({ users });
+})
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+router.get("/:username", ensureCorrectUser, async function (req, res, next) {
+    const username = req.params.username;
+    const user = await User.get(username);
+    return res.json({ user });
+})
 
 
 /** GET /:username/to - get messages to user
@@ -27,11 +36,11 @@ const User = require("../models/user");
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-router.get("/:username/to", async function(req, res, next) {
+router.get("/:username/to", ensureCorrectUser, async function (req, res, next) {
     const username = req.params.username;
-    const user = await User.messagesTo(username);
-    console.log("user:", user);
-    return res.json({ 'messages' : user});
+    const messages = await User.messagesTo(username);
+    // console.log("user:", user);
+    return res.json({ messages });
 })
 
 /** GET /:username/from - get messages from user
@@ -43,5 +52,10 @@ router.get("/:username/to", async function(req, res, next) {
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get("/:username/from", ensureCorrectUser, async function (req, res, next) {
+    const username = req.params.username;
+    const messages = await User.messagesFrom(username);
+    return res.json({ messages });
+})
 
 module.exports = router;
